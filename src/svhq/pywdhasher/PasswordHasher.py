c1470d0c5674ddd5ee22384520ca70a0258af63b
Anders Krarup <anderskrarup@HOLCOs-MacBook-Pro-2.local>
2019-07-03 11:31:52 +0200

    almost able to add pass to clipboard

--- src/svhq/pywdhasher/PasswordHasher.py
+++ src/svhq/pywdhasher/PasswordHasher.py
@@ -18,10 +18,12 @@ It defines classes_and_methods
 '''
 
 import sys
-import os
+import getpass
+import hashlib
+import time
 
 from argparse import ArgumentParser
-from argparse import RawDescriptionHelpFormatter
+from Tkinter import Tk
 
 __all__ = []
 __version__ = 0.1
@@ -50,86 +52,51 @@ def main(argv=None): # IGNORE:C0111
     else:
         sys.argv.extend(argv)
 
-    program_name = os.path.basename(sys.argv[0])
-    program_version = "v%s" % __version__
-    program_build_date = str(__updated__)
-    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
-    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
-    program_license = '''%s
+    # Setup argument parser
+    parser = ArgumentParser()
+    parser.add_argument("-o", "--out", action="store_true", help="output password in tty instead of inserting in clipboard")
+    parser.add_argument("-r", "--rule", help="transform rule(s)")
+    
+    # clipboard manager
+    r = Tk()
 
-  Created by user_name on %s.
-  Copyright 2019 organization_name. All rights reserved.
+    # Process arguments
+    args = parser.parse_args()
+    salt = getpass.getpass("salt: ")
+    domain = r.clipboard_get()
 
-  Licensed under the Apache License 2.0
-  http://www.apache.org/licenses/LICENSE-2.0
+    password = hashlib.md5(domain + salt).hexdigest()
 
-  Distributed on an "AS IS" basis without warranties
-  or conditions of any kind, either express or implied.
+    if args.out is True:
+        print "password for domain '" + domain + "':"
+        print password
+    else:
+        r.clipboard_append(password)
+        print "password for domain '" + domain + "' is added to clipboard..."
+        r.update()
+        r.destroy()
+#        time.sleep(1)
+        raw_input("Paste password into password-field, then hit Enter to exit")
 
-USAGE
-''' % (program_shortdesc, str(__date__))
-
-    try:
-        # Setup argument parser
-        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-r", "--recursive", dest="recurse", action="store_true", help="recurse into subfolders [default: %(default)s]")
-        parser.add_argument("-v", "--verbose", dest="verbose", action="count", help="set verbosity level [default: %(default)s]")
-        parser.add_argument("-i", "--include", dest="include", help="only include paths matching this regex pattern. Note: exclude is given preference over include. [default: %(default)s]", metavar="RE" )
-        parser.add_argument("-e", "--exclude", dest="exclude", help="exclude paths matching this regex pattern. [default: %(default)s]", metavar="RE" )
-        parser.add_argument('-V', '--version', action='version', version=program_version_message)
-        parser.add_argument(dest="paths", help="paths to folder(s) with source file(s) [default: %(default)s]", metavar="path", nargs='+')
-
-        # Process arguments
-        args = parser.parse_args()
-
-        paths = args.paths
-        verbose = args.verbose
-        recurse = args.recurse
-        inpat = args.include
-        expat = args.exclude
-
-        if verbose > 0:
-            print("Verbose mode on")
-            if recurse:
-                print("Recursive mode on")
-            else:
-                print("Recursive mode off")
-
-        if inpat and expat and inpat == expat:
-            raise CLIError("include and exclude pattern are equal! Nothing will be processed.")
-
-        for inpath in paths:
-            ### do something with inpath ###
-            print(inpath)
-        return 0
-    except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
-        return 0
-    except Exception, e:
-        if DEBUG or TESTRUN:
-            raise(e)
-        indent = len(program_name) * " "
-        sys.stderr.write(program_name + ": " + repr(e) + "\n")
-        sys.stderr.write(indent + "  for help use --help")
-        return 2
+    return 0
 
 if __name__ == "__main__":
-    if DEBUG:
-        sys.argv.append("-h")
-        sys.argv.append("-v")
-        sys.argv.append("-r")
-    if TESTRUN:
-        import doctest
-        doctest.testmod()
-    if PROFILE:
-        import cProfile
-        import pstats
-        profile_filename = 'svhq.pywdhasher.PasswordHasher_profile.txt'
-        cProfile.run('main()', profile_filename)
-        statsfile = open("profile_stats.txt", "wb")
-        p = pstats.Stats(profile_filename, stream=statsfile)
-        stats = p.strip_dirs().sort_stats('cumulative')
-        stats.print_stats()
-        statsfile.close()
-        sys.exit(0)
+#     if DEBUG:
+#         sys.argv.append("-h")
+#         sys.argv.append("-v")
+#         sys.argv.append("-r")
+#     if TESTRUN:
+#         import doctest
+#         doctest.testmod()
+#     if PROFILE:
+#         import cProfile
+#         import pstats
+#         profile_filename = 'svhq.pywdhasher.PasswordHasher_profile.txt'
+#         cProfile.run('main()', profile_filename)
+#         statsfile = open("profile_stats.txt", "wb")
+#         p = pstats.Stats(profile_filename, stream=statsfile)
+#         stats = p.strip_dirs().sort_stats('cumulative')
+#         stats.print_stats()
+#         statsfile.close()
+#         sys.exit(0)
     sys.exit(main())
